237208430966efa8552bdd8c489028ad7bdd3eaa
EKravtz32 <ekravtz32@ekravtz32s-imac.local>
2016-02-19 17:59:50 +0200

    Initial Commit

---
+++ PizzaApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PizzaApp
+//
+//  Created by EKravtz32 on 19.02.16.
+//  Copyright © 2016 EvgenKravtsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PizzaApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PizzaApp
+//
+//  Created by EKravtz32 on 19.02.16.
+//  Copyright © 2016 EvgenKravtsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
